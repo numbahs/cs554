55f0e5b742c3622d6663fe6b9de0428d6e8841b3
numbahs <panyba@gmail.com>
2018-02-08 22:49:44 -0500

    zipped and changed some css stuff

--- scripts/zip.js
+++ scripts/zip.js
@@ -3,7 +3,7 @@ const lab = process.argv.slice(-1)[0]
 
 if(shell.ls().indexOf(`Assignment${lab}`) !== -1) {
     shell.cd(`./Assignment${lab}`)
-    shell.exec(`zip -r Tang_Albert_CS546_WS.zip . -x "*node_modules/" -x "node_modules/**" -x "*.DS_Store" -x "*.zip"`)
+    shell.exec(`zip -r Tang_Albert_CS554.zip . -x "*node_modules/" -x "node_modules/**" -x "*.DS_Store" -x "*.zip"`)
 } else {
     console.log(`Assignment${lab} folder must already exist!`);
 }
