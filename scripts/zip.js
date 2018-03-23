c29e8cc82450ceb2a9b23f4eee9c446e183d711a
numbahs <panyba@gmail.com>
2018-03-22 21:42:39 -0400

    zipping with assignment now for sanity

--- scripts/zip.js
+++ scripts/zip.js
@@ -4,7 +4,7 @@ const lab = process.argv.slice(-1)[0];
 if (shell.ls().indexOf(`${lab}`) !== -1) {
   shell.cd(`./${lab}`);
   shell.exec(
-    `zip -r Tang_Albert_CS554.zip . -x "*node_modules/" -x "node_modules/**" -x "*.DS_Store" -x "*.zip" -x ".vscode/*"`
+    `zip -r Tang_Albert_CS554_${lab}.zip . -x "*node_modules/" -x "node_modules/**" -x "*.DS_Store" -x "*.zip" -x ".vscode/*"`
   );
 } else {
   console.log(`${lab} folder must already exist!`);
