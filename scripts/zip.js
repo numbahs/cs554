3087da766aad1a371d4fb9d1990dd9e06246402d
numbahs <panyba@gmail.com>
2018-03-22 20:24:00 -0400

    no .vscode in zips

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,9 +1,11 @@
 const shell = require('shelljs');
-const lab = process.argv.slice(-1)[0]
+const lab = process.argv.slice(-1)[0];
 
-if(shell.ls().indexOf(`${lab}`) !== -1) {
-    shell.cd(`./${lab}`)
-    shell.exec(`zip -r Tang_Albert_CS554.zip . -x "*node_modules/" -x "node_modules/**" -x "*.DS_Store" -x "*.zip"`)
+if (shell.ls().indexOf(`${lab}`) !== -1) {
+  shell.cd(`./${lab}`);
+  shell.exec(
+    `zip -r Tang_Albert_CS554.zip . -x "*node_modules/" -x "node_modules/**" -x "*.DS_Store" -x "*.zip" -x ".vscode/*"`
+  );
 } else {
-    console.log(`${lab} folder must already exist!`);
+  console.log(`${lab} folder must already exist!`);
 }
